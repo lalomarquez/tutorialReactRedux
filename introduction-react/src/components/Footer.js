6e301d9bc72e0601ebce69fb9cb16b9bcbd4537f
Eduardo Márquez Espinosa <eduardo.marquez@PSWGLOBAL.DOM>
2018-04-19 13:02:42 -0500

    add bootstrap & fontawesome

---
+++ introduction-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as FontAwesome from 'react-icons/lib/fa'
+
+export const Footer = () => {
+    return (
+        <div>
+            <p>this is the footer component</p>
+            <FontAwesome.FaUser />
+        </div>
+    )
+}
--- introduction-react/src/index.js
+++ introduction-react/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
